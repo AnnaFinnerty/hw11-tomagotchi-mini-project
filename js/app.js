a45fe9e91691daaf62d353944cc9a2fbb235814e
AnnaFinnerty <algfinnerty@gmail.com>
2019-10-02 15:23:53 -0600

    added movement + boundary detection

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log("app running");
 
+$('body').attr('backgroundImage','url("img/jungle_background.jpg")')
+
 $('#feed-button').on('click', function(){
     game.feedPet();
 })
@@ -12,20 +14,38 @@ $('#lights-button').on('click', function(){
     game.toggleLights();
 })
 
+$('.pet-container').on('mousemove', function(e){
+    game.movePet(e.clientX,e.clientY);
+})
+
 class Pet{
-    constructor(name,age){
+    constructor(name,age,$display){
         this.name = name;
         this.ageYear = age;
         this.ageMonth = 0;
-        this.hunger = 0;
-        this.sleepiness = 0;
-        this.boredom = 0;
+        this.$display = $display;
+        this.width = $display.width();
+        this.height = $display.height();
+        console.log(this.width,this.height);
+        this.traits = {
+            hunger: 0,
+            sleepiness: 0,
+            boredom: 0,
+        }
+        this.boundaries = {
+            left: 0,
+            right: $('.pet-container').width(),
+            top: 0,
+            bottom: $('.pet-container').height(),
+        }
+        
         this.state = "sitting";
+        this.isDead = false;
     }
     age(){
         //console.log("I am aging!");
         if(this.state === "sleeping"){
-            this.sleepiness = this.sleepiness-- > 0 ? 0: this.sleepiness--;
+            this.traits.sleepiness = this.traits.sleepiness-- > 0 ? 0: this.traits.sleepiness--;
         }
         if(this.ageMonth === 11){
             this.ageMonth = 0;
@@ -33,25 +53,48 @@ class Pet{
         } else {
             this.ageMonth++;
         }
+        this.gremlin();
+    }
+    gremlin(){
+        const chanceSomethingHappens = Math.random();
+        //mtc increase with age?
+        if(chanceSomethingHappens < .5){
+            const traits = Object.keys(this.traits);
+            const r = Math.floor(Math.random()*traits.length);
+            this.traits[traits[r]] += 1;
+            if(this.traits[traits[r]] >= 10){
+                this.die();
+            }
+        }
+    }
+    updatePosition(x,y){
+        //prevent pet from hitting boundaries
+        x = x + this.width > this.boundaries.right ? this.boundaries.right - this.width  : x;
+        y = y + this.height > this.boundaries.bottom ? this.boundaries.bottom - this.height : y;
+        this.$display.css("left",x+"px");
+        this.$display.css("top",y+"px");
     }
     feed(){
         console.log("I'm being fed!");
         this.state = "sitting";
-        this.hunger = this.hunger-- > 0 ? 0: this.hunger--;
+        this.hunger = this.traits.hunger-- > 0 ? 0: this.traits.hunger--;
     }
     play(){
         console.log("I'm playing");
-        this.state = "standing";
-        this.boredom = this.boredom-- > 0 ? 0: this.boredom--;
+        this.state = "playing";
+        this.boredom = this.traits.boredom-- > 0 ? 0: this.traits.boredom--;
     }
     sleep(){
         console.log("I'm sleeping");
         this.state = "sleeping";
     }
+    die(){
+        this.isDead = true;
+    }
 }
 
 const game = {
-    ageRate: 200,
+    ageRate: 1000,
     timer: null,
     lightsOn: true,
     pet: null,
@@ -78,16 +121,24 @@ const game = {
         $pet.attr("src","img/tiger_seated.png")
         this.ui.$petContainer.append($pet);
         this.ui.$petImage = $pet;
-        this.pet = new Pet("Argus",0);
+        this.pet = new Pet("Argus",0, $pet);
     },
     startTimer(){
         this.timer = setInterval(()=>{
-            this.pet.age();
-            this.updateUI();
+            if(this.pet.isDead){
+                this.stopTimer();
+                //console.log("pet has died");
+            } else {
+                this.pet.age();
+                this.updateUI();
+            }
         },this.ageRate)
     },
     stopTimer(){
         clearInterval(this.timer);
+    },
+    movePet(x,y){
+        this.pet.updatePosition(x,y);
     },
     feedPet(){
         console.log("feeding pet");
@@ -113,9 +164,11 @@ const game = {
 
     },
     updateUI(){
-        this.ui.$boredomDisplay.text(this.pet.boredom);
-        this.ui.$sleepinessDisplay.text(this.pet.sleepiness);
-        this.ui.$hungerDisplay.text(this.pet.hunger);
+        this.ui.$boredomDisplay.text(this.pet.traits.boredom);
+        this.ui.$sleepinessDisplay.text(this.pet.traits.sleepiness);
+        this.ui.$hungerDisplay.text(this.pet.traits.hunger);
+        this.ui.$ageYear.text(this.pet.ageYear);
+        this.ui.$ageMonth.text(this.pet.ageMonth+1);
     },
     clearContainer(container){
         while(container.firstChild){
